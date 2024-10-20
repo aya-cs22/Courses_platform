cd5211e6b9f06696d99449db7dd2284cbed5f399
aya-cs22 <aya786930@gmail.com>
2024-10-20 16:55:15 +0300

    server & server

--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config({ path: 'config.env' });
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+
+dotenv.config({ path: 'config.env' });
+const dbConnection = require('./config/db');
 
 // Connect with DB
-const dbConnection = require('./config/db');
 dbConnection();
 
 // express app
 const app = express();
 
 // middlewares
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -28,6 +33,11 @@ app.use('/api/users', userRoutes);
 app.use('/api/course', courseRoutes);
 app.use('/api/groups', groupsRoutes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
