76aba6e511c08722e21e2a35bfe338a3fe0780e4
aya-cs22 <aya786930@gmail.com>
2024-10-25 19:30:27 +0300

    delet backend

--- back-end/config/mailConfig.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE,
-  auth: {
-    user: process.env.ADMIN_EMAIL,
-    pass: process.env.EMAIL_PASS
-  }
-});
-module.exports = transporter;
